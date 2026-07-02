a41675267e112a6eecea71c24632cd98901f76de
ChristianLeinen <c.leinen@radix-it.de>
2026-07-03 01:17:26 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace BinaryClock
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            hours = new Label();
+            contextMenuStrip1 = new ContextMenuStrip(components);
+            horizontalToolStripMenuItem = new ToolStripMenuItem();
+            verticalToolStripMenuItem = new ToolStripMenuItem();
+            toolStripMenuItem1 = new ToolStripSeparator();
+            toolStripMenuItem2 = new ToolStripMenuItem();
+            toolStripSeparator1 = new ToolStripSeparator();
+            closeToolStripMenuItem = new ToolStripMenuItem();
+            minutes = new Label();
+            seconds = new Label();
+            timer1 = new System.Windows.Forms.Timer(components);
+            smallToolStripMenuItem = new ToolStripMenuItem();
+            mediumToolStripMenuItem = new ToolStripMenuItem();
+            largeToolStripMenuItem = new ToolStripMenuItem();
+            contextMenuStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // hours
+            // 
+            hours.BorderStyle = BorderStyle.FixedSingle;
+            hours.ContextMenuStrip = contextMenuStrip1;
+            hours.Font = new Font("Consolas", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            hours.ForeColor = Color.FromArgb(0, 192, 0);
+            hours.Location = new Point(5, 5);
+            hours.Margin = new Padding(6, 0, 6, 0);
+            hours.Name = "hours";
+            hours.Size = new Size(180, 40);
+            hours.TabIndex = 0;
+            hours.Text = "●○ ●○● h";
+            hours.TextAlign = ContentAlignment.TopRight;
+            hours.MouseCaptureChanged += Form1_MouseCaptureChanged;
+            hours.MouseDown += Form1_MouseDown;
+            hours.MouseMove += Form1_MouseMove;
+            hours.MouseUp += Form1_MouseUp;
+            // 
+            // contextMenuStrip1
+            // 
+            contextMenuStrip1.ImageScalingSize = new Size(20, 20);
+            contextMenuStrip1.Items.AddRange(new ToolStripItem[] { horizontalToolStripMenuItem, verticalToolStripMenuItem, toolStripMenuItem1, toolStripMenuItem2, toolStripSeparator1, closeToolStripMenuItem });
+            contextMenuStrip1.Name = "contextMenuStrip1";
+            contextMenuStrip1.Size = new Size(211, 148);
+            // 
+            // horizontalToolStripMenuItem
+            // 
+            horizontalToolStripMenuItem.Checked = true;
+            horizontalToolStripMenuItem.CheckState = CheckState.Checked;
+            horizontalToolStripMenuItem.Name = "horizontalToolStripMenuItem";
+            horizontalToolStripMenuItem.Size = new Size(210, 26);
+            horizontalToolStripMenuItem.Text = "&Horizontal";
+            horizontalToolStripMenuItem.Click += horizontalToolStripMenuItem_Click;
+            // 
+            // verticalToolStripMenuItem
+            // 
+            verticalToolStripMenuItem.Name = "verticalToolStripMenuItem";
+            verticalToolStripMenuItem.Size = new Size(210, 26);
+            verticalToolStripMenuItem.Text = "&Vertical";
+            verticalToolStripMenuItem.Click += verticalToolStripMenuItem_Click;
+            // 
+            // toolStripMenuItem1
+            // 
+            toolStripMenuItem1.Name = "toolStripMenuItem1";
+            toolStripMenuItem1.Size = new Size(207, 6);
+            // 
+            // toolStripMenuItem2
+            // 
+            toolStripMenuItem2.DropDownItems.AddRange(new ToolStripItem[] { smallToolStripMenuItem, mediumToolStripMenuItem, largeToolStripMenuItem });
+            toolStripMenuItem2.Name = "toolStripMenuItem2";
+            toolStripMenuItem2.Size = new Size(210, 26);
+            toolStripMenuItem2.Text = "&Size";
+            // 
+            // toolStripSeparator1
+            // 
+            toolStripSeparator1.Name = "toolStripSeparator1";
+            toolStripSeparator1.Size = new Size(207, 6);
+            // 
+            // closeToolStripMenuItem
+            // 
+            closeToolStripMenuItem.Name = "closeToolStripMenuItem";
+            closeToolStripMenuItem.ShortcutKeys = Keys.Alt | Keys.F4;
+            closeToolStripMenuItem.Size = new Size(210, 26);
+            closeToolStripMenuItem.Text = "&Close";
+            closeToolStripMenuItem.Click += closeToolStripMenuItem_Click;
+            // 
+            // minutes
+            // 
+            minutes.BorderStyle = BorderStyle.FixedSingle;
+            minutes.ContextMenuStrip = contextMenuStrip1;
+            minutes.Font = new Font("Consolas", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            minutes.ForeColor = Color.FromArgb(0, 192, 0);
+            minutes.Location = new Point(190, 5);
+            minutes.Margin = new Padding(6, 0, 6, 0);
+            minutes.Name = "minutes";
+            minutes.Size = new Size(180, 40);
+            minutes.TabIndex = 1;
+            minutes.Text = "○●○ ●○● m";
+            minutes.TextAlign = ContentAlignment.TopRight;
+            minutes.MouseDown += Form1_MouseDown;
+            minutes.MouseMove += Form1_MouseMove;
+            minutes.MouseUp += Form1_MouseUp;
+            // 
+            // seconds
+            // 
+            seconds.BorderStyle = BorderStyle.FixedSingle;
+            seconds.ContextMenuStrip = contextMenuStrip1;
+            seconds.Font = new Font("Consolas", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            seconds.ForeColor = Color.FromArgb(0, 192, 0);
+            seconds.Location = new Point(375, 5);
+            seconds.Margin = new Padding(6, 0, 6, 0);
+            seconds.Name = "seconds";
+            seconds.Size = new Size(180, 40);
+            seconds.TabIndex = 2;
+            seconds.Text = "○●○ ●○● s";
+            seconds.TextAlign = ContentAlignment.TopRight;
+            seconds.MouseDown += Form1_MouseDown;
+            seconds.MouseMove += Form1_MouseMove;
+            seconds.MouseUp += Form1_MouseUp;
+            // 
+            // timer1
+            // 
+            timer1.Enabled = true;
+            timer1.Tick += timer1_Tick;
+            // 
+            // smallToolStripMenuItem
+            // 
+            smallToolStripMenuItem.Name = "smallToolStripMenuItem";
+            smallToolStripMenuItem.Size = new Size(224, 26);
+            smallToolStripMenuItem.Text = "&Small";
+            smallToolStripMenuItem.Click += smallToolStripMenuItem_Click;
+            // 
+            // mediumToolStripMenuItem
+            // 
+            mediumToolStripMenuItem.Checked = true;
+            mediumToolStripMenuItem.CheckState = CheckState.Checked;
+            mediumToolStripMenuItem.Name = "mediumToolStripMenuItem";
+            mediumToolStripMenuItem.Size = new Size(224, 26);
+            mediumToolStripMenuItem.Text = "&Medium";
+            mediumToolStripMenuItem.Click += mediumToolStripMenuItem_Click;
+            // 
+            // largeToolStripMenuItem
+            // 
+            largeToolStripMenuItem.Name = "largeToolStripMenuItem";
+            largeToolStripMenuItem.Size = new Size(224, 26);
+            largeToolStripMenuItem.Text = "&Large";
+            largeToolStripMenuItem.Click += largeToolStripMenuItem_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(16F, 36F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Black;
+            ClientSize = new Size(560, 50);
+            ContextMenuStrip = contextMenuStrip1;
+            Controls.Add(seconds);
+            Controls.Add(minutes);
+            Controls.Add(hours);
+            Font = new Font("Consolas", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            FormBorderStyle = FormBorderStyle.None;
+            Margin = new Padding(6);
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "Form1";
+            Padding = new Padding(5);
+            Text = "Binary Clock";
+            TopMost = true;
+            Load += Form1_Load;
+            MouseCaptureChanged += Form1_MouseCaptureChanged;
+            MouseDown += Form1_MouseDown;
+            MouseMove += Form1_MouseMove;
+            MouseUp += Form1_MouseUp;
+            contextMenuStrip1.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Label hours;
+        private Label minutes;
+        private Label seconds;
+        private System.Windows.Forms.Timer timer1;
+        private ContextMenuStrip contextMenuStrip1;
+        private ToolStripMenuItem closeToolStripMenuItem;
+        private ToolStripMenuItem horizontalToolStripMenuItem;
+        private ToolStripMenuItem verticalToolStripMenuItem;
+        private ToolStripSeparator toolStripMenuItem1;
+        private ToolStripMenuItem toolStripMenuItem2;
+        private ToolStripSeparator toolStripSeparator1;
+        private ToolStripMenuItem smallToolStripMenuItem;
+        private ToolStripMenuItem mediumToolStripMenuItem;
+        private ToolStripMenuItem largeToolStripMenuItem;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,204 @@
+﻿using System.Diagnostics;
+using System.Text;
+
+namespace BinaryClock
+{
+    public partial class Form1 : Form
+    {
+        internal enum TimeComp
+        {
+            Hour,
+            Minute,
+            Second
+        };
+
+        internal enum FontSize : int
+        {
+            Small = 12,
+            Medium = 18,
+            Large = 24
+        }
+
+        private int fontSize = (int)FontSize.Medium;
+        private bool isVertical;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.ChangeLayout(this.isVertical, this.fontSize);
+        }
+
+        private void ChangeLayout(bool isVertical, int fontSize)
+        {
+            this.isVertical = isVertical;
+            this.fontSize = fontSize;
+            this.UpdateLayout();
+            this.UpdateMenuChecks();
+        }
+
+        private void UpdateLayout()
+        {
+            // measure required size for the contents of one label
+            using var gfx = Graphics.FromHwnd(this.Handle);
+            using var font = new Font(this.Font.FontFamily, this.fontSize, FontStyle.Bold);
+            var size = gfx.MeasureString(" 00 0000 0", font).ToSize();
+
+            // resize labels + fonts
+            this.hours.Font = this.minutes.Font = this.seconds.Font = font;
+            this.hours.Size = this.minutes.Size = this.seconds.Size = size;
+
+            // resize form
+            if (this.isVertical)
+            {
+                size.Height *= 3;
+                size.Height += 2 * this.Padding.Vertical;
+                size.Width += this.Padding.Horizontal;
+            }
+            else
+            {
+                size.Width *= 3;
+                size.Width += 2 * this.Padding.Horizontal;
+                size.Height += this.Padding.Vertical;
+            }
+            this.Size = size;
+
+            // reposition label controls
+            this.hours.Location = new Point(this.Padding.Left, this.Padding.Top);
+            if (this.isVertical)
+            {
+                this.minutes.Location = new Point(this.Padding.Left, this.hours.Location.Y + this.hours.Height + (this.Padding.Vertical >> 1));
+                this.seconds.Location = new Point(this.Padding.Left, this.ClientSize.Height - this.seconds.Height - this.Padding.Bottom);
+            }
+            else
+            {
+                this.minutes.Location = new Point(this.hours.Location.X + this.hours.Size.Width + (this.Padding.Horizontal >> 1), this.Padding.Top);
+                this.seconds.Location = new Point(this.ClientSize.Width - this.seconds.Width - this.Padding.Right, this.Padding.Top);
+            }
+        }
+
+        private static string FormatTime(DateTime time, TimeComp timeComp)
+        {
+            var value = 0;
+            var digits = 0;
+            switch (timeComp)
+            {
+                case TimeComp.Hour:
+                    value = time.Hour;
+                    digits = 5;
+                    break;
+                case TimeComp.Minute:
+                    value = time.Minute;
+                    digits = 6;
+                    break;
+                case TimeComp.Second:
+                    value = time.Second;
+                    digits = 6;
+                    break;
+                default:
+                    throw new ArgumentException($"unknown Time component: {timeComp}", nameof(timeComp));
+            }
+
+            var result = new StringBuilder(16);
+            // run through bits
+            while (digits-- > 0)
+            {
+                var pos = 0x01 << digits;
+                // space out nibbles
+                if (digits == 3)
+                {
+                    result.Append(' ');
+                }
+                result.Append((value & pos) > 0 ? '●' : '○');
+            }
+
+            switch (timeComp)
+            {
+                case TimeComp.Hour:
+                    result.Append(" h");
+                    break;
+                case TimeComp.Minute:
+                    result.Append(" m");
+                    break;
+                case TimeComp.Second:
+                    result.Append(" s");
+                    break;
+            }
+            return result.ToString();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            var time = DateTime.Now;
+            this.hours.Text = FormatTime(time, TimeComp.Hour);
+            this.minutes.Text = FormatTime(time, TimeComp.Minute);
+            this.seconds.Text = FormatTime(time, TimeComp.Second);
+        }
+
+        #region Mouse stuff
+        private Point LastLocation { get; set; }
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (sender is Control ctl)
+            {
+                this.Capture = true;
+                this.LastLocation = ctl.PointToScreen(e.Location);
+            }
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (!this.Capture || sender is not Control ctl)
+            {
+                return;
+            }
+            // calculate diff
+            var currentLocation = ctl.PointToScreen(e.Location);
+            var diff = currentLocation - ((Size)this.LastLocation);
+            // calculate new position
+            var newPos = this.Location;
+            newPos.Offset(diff);
+            this.Location = newPos;
+            // remember current position
+            this.LastLocation = currentLocation;
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            this.Capture = false;
+        }
+
+        private void Form1_MouseCaptureChanged(object sender, EventArgs e)
+        {
+            Debug.WriteLine("Form1_MouseCaptureChanged");
+        }
+        #endregion
+
+        #region Menu stuff
+        private void UpdateMenuChecks()
+        {
+            this.horizontalToolStripMenuItem.Checked = !this.isVertical;
+            this.verticalToolStripMenuItem.Checked = this.isVertical;
+
+            this.smallToolStripMenuItem.Checked = this.fontSize == (int)FontSize.Small;
+            this.mediumToolStripMenuItem.Checked = this.fontSize == (int)FontSize.Medium;
+            this.largeToolStripMenuItem.Checked = this.fontSize == (int)FontSize.Large;
+        }
+
+        private void horizontalToolStripMenuItem_Click(object sender, EventArgs e) => this.ChangeLayout(false, this.fontSize);
+
+        private void verticalToolStripMenuItem_Click(object sender, EventArgs e) => this.ChangeLayout(true, this.fontSize);
+
+        private void smallToolStripMenuItem_Click(object sender, EventArgs e) => this.ChangeLayout(this.isVertical, (int)FontSize.Small);
+
+        private void mediumToolStripMenuItem_Click(object sender, EventArgs e) => this.ChangeLayout(this.isVertical, (int)FontSize.Medium);
+
+        private void largeToolStripMenuItem_Click(object sender, EventArgs e) => this.ChangeLayout(this.isVertical, (int)FontSize.Large);
+
+        private void closeToolStripMenuItem_Click(object sender, EventArgs e) => this.Close();
+        #endregion
+    }
+}
